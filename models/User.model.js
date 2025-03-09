ce31cd0c9e54114f289d8245d7e3382cd29e7dba
Pujan-khunt <pujankhunt2412@gmail.com>
2025-03-09 20:43:14 +0530

    Added documentation for User Model and its methods

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
+/**
+ * User Schema
+ * 
+ * schema for User Model in the database.
+ * It includes fields for username, password, and refresh tokens along with the timestamps.
+ * 
+ * @typedef {Object} UserSchema
+ * @property {string} username - The username of the user. It is required, unique, trimmed, and stored in lowercase.
+ * @property {string} password - The password of the user. It is required and will not be selected automatically unless explicitly mentioned.
+ * @property {string[]} refreshTokens - An array of refresh tokens associated with the user. Defaults to an empty array.
+ * @property {Date} createdAt - The timestamp when the user was created.
+ * @property {Date} updatedAt - The timestamp when the user was last updated.
+ */
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -12,7 +25,7 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
-    select: false // wont automatically select the password if not mentioned explicitly
+    select: false
   },
   refreshTokens: {
     type: [String],
@@ -20,7 +33,12 @@ const UserSchema = new Schema({
   }
 }, { timestamps: true });
 
-// Hash passwords whenever they are modified or set for the first time.
+/**
+ * Middleware to hash passwords whenever they are modified or set for the first time.
+ * 
+ * @param {Function} next - The next middleware function in the stack.
+ * @returns {void}
+ */
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) { return next(); }
   try {
@@ -31,26 +49,26 @@ UserSchema.pre("save", async function (next) {
   }
 });
 
+/**
+ * Compares the provided password with the stored hashed password.
+ * 
+ * @param {string} providedPassword - The password to compare against.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the passwords match.
+ */
 UserSchema.methods.comparePassword = async function (providedPassword) {
   return await bcrypt.compare(providedPassword, this.password);
 };
 
+/**
+ * Middleware to hash refresh tokens whenever they are modified or set for the first time.
+ * 
+ * @param {Function} next - The next middleware function in the stack.
+ * @returns {void}
+ */
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("refreshTokens")) { return next(); }
 
-  // Promise.all will wait until either every single promise is resolved or one of them is rejected.
-  // We want that every single refreshToken which isn't hashed to get hashed for that we will need to wait for all the Promises which are in pending state.
-  // .map function will return something like this.
-  // [
-  //   Promise { <pending> }, // If hashing was needed
-  //   Promise { <fulfilled>: "hashedToken123..." }, // If already hashed, original token is returned
-  //   Promise { <fulfilled>: "hashedToken123..." }, 
-  // ]
-  // As we need to wait for the pending promises to get resolved that is why we use Promise.all();
   this.refreshTokens = await Promise.all(
-    // This map will return an array of promises, where all values will be something like this: Promise.resolve(refreshToken)
-    // There will be one exception where the refreshToken is not hashed. The .map function will not wait for it to resolve it will
-    // Instantly return an array of promises.
     this.refreshTokens.map(async (refreshToken) => {
       // Check if it's already hashed (bcrypt hashes start with $2b$)
       const alreadyHashed = refreshToken.startsWith("$2b$");
@@ -66,7 +84,12 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 
-// Tells whether the provided refresh token exists with the user or not.
+/**
+ * Returns true if the provided refresh token exists in the database, otherwise it returns false.
+ * 
+ * @param {string} providedRefreshToken - The refresh token to compare against.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the presence of refreshToken in the array.
+ */
 UserSchema.methods.compareRefreshToken = async function (providedRefreshToken) {
   const results = await Promise.all(
     // Will return an array of promises.(handled by Promise.all())
@@ -77,10 +100,15 @@ UserSchema.methods.compareRefreshToken = async function (providedRefreshToken) {
   return results.includes(true);
 };
 
-// Goal of this method is to provide the index of the refreshToken in the array it is stored.
-UserSchema.methods.getRefreshTokenIndex = async function (refreshToken) {
+/**
+ * Returns the index of the refresh token in the array.
+ * 
+ * @param {string} providedRefreshToken - The refresh token to compare against.
+ * @returns {Promise<number>} - A promise that resolves to the index of the provided refresh token in the array.
+ */
+UserSchema.methods.getRefreshTokenIndex = async function (providedRefreshToken) {
   const results = await Promise.all(
-    this.refreshToken.map((refreshToken, index) => bcrypt.compare(refreshToken, providedPassword))
+    this.refreshToken.map((refreshToken) => bcrypt.compare(refreshToken, providedRefreshToken))
   );
 
   console.log(results);
